ccad9c2167f257e41f98753ee06f54027de8ec61
jack <jackdek11@gmail.com>
2022-10-11 21:29:58 +0200

    updateed question 2

--- src/MIPs/question2.asm
+++ src/MIPs/question2.asm
@@ -1,8 +1,11 @@
 .data
     prompt_1:    .asciiz "Enter a sum:\n"
     prompt_2:    .asciiz "The value is:\n"
-    user_input:  .space 256        # string containing sum to calculate
-    answer:      .space 256        # integer answer
+    user_input:    .space 256        # string containing sum to calculate
+    answer:    .byte 0        # integer answer
+    position:    .byte 0
+    currentValue: .byte 0
+    null:        .asciiz ""
 .text
 
 .globl main
@@ -18,7 +21,38 @@ main:
     li    $a1, 255
     li    $v0, 8
     syscall
+
+    # let $s0 point to position
+
+    # $s1 points to currentValue
+
+    # $s2 points to null
+
+    # $s3 points to answer
+
+    # $s4 points to user_input
     
+    jar    calcSum    #1+2+3
+
+    # print answer
+
+calcSum:
+    # if value at position is null character then we jump back to main
+    # make position point to next +
+    jar    findPlus
+    # now we want to read in the number up to the position
+    # store that in currentValue
+    # add currentValue to answer
+    
+
+findPlus:
+    # reads in bit at position
+    
+    # if it is a null character, TODO
+
+    # if bit is plus
+        # return position
+    # else increment position and call findPlus
     
 
 exit:
